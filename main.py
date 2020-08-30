abe8d775c1b0fbae46229e80bf6dca95724788aa
ciskoh <matteo.jriva@gmail.com>
2020-08-30 22:13:40 +0200

    functions

--- main.py
+++ main.py
@@ -14,21 +14,27 @@ import os
 # Main variables and parameters
 ds_path = os.getcwd() + "/data_source"
 out_path = os.getcwd() + "/output"
+ita_url = "https://github.com/pcm-dpc/COVID-19/blob/master/dati-regioni/dpc-covid19-ita-regioni.csv"
+ch_url = "https://github.com/openZH/covid_19/blob/master/fallzahlen_kanton_total_csv_v2/COVID19_Fallzahlen_FL_total.csv"
 
-print(f" cwd is {os.getcwd()}; ds path is {ds_path}")
 
 # startup check
 # Check that files and folders are available
-if not os.path.isdir(ds_path):
-    print("no data source folder, creating one and downloading files")
-    os.mkdir(ds_path)
+def check_folders():
+    if not os.path.isdir(ds_path):
+        print("no data source folder, creating one and downloading files")
+        os.mkdir(ds_path)
 
-if not os.path.isdir(out_path):
-    print("no output folder, creating one ")
-    os.mkdir(out_path)
+    if not os.path.isdir(out_path):
+        print("no output folder, creating one")
+        os.mkdir(out_path)
 
 
 # 1 download data_source file
+def download_recent_data( country, date, url):
+    data_path = ds_path+f"/{country}_{date}.csv"
+    if not os.path.exists(data_path): 
+
 
 # 2 clean and import data
 
